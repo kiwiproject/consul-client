43fe6edfea1cb9e1857711907cd8c3e1150afa5c
Scott Leberknight <174812+sleberknight@users.noreply.github.com>
2023-05-18 12:11:41 -0400

    Fix Sonar "blocker" issues (tests with no assertions) (#60)

--- src/test/java/com/orbitz/consul/model/catalog/TaggedAddressesTest.java
+++ src/test/java/com/orbitz/consul/model/catalog/TaggedAddressesTest.java
@@ -1,22 +1,33 @@
 package com.orbitz.consul.model.catalog;
 
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+
 import org.junit.Test;
+
+import java.util.Optional;
 
 public class TaggedAddressesTest {
 
     @Test
     public void buildingTaggedAddressWithAllAttributesShouldSucceed() {
-        ImmutableTaggedAddresses.builder()
+        ImmutableTaggedAddresses taggedAddresses = ImmutableTaggedAddresses.builder()
                 .lan("127.0.0.1")
                 .wan("172.217.17.110")
                 .build();
+
+        assertThat(taggedAddresses.getLan(), is(Optional.of("127.0.0.1")));
+        assertThat(taggedAddresses.getWan(), is("172.217.17.110"));
     }
 
     @Test
     public void buildingTaggedAddressWithoutLanAddressShouldSucceed() {
-        ImmutableTaggedAddresses.builder()
+        ImmutableTaggedAddresses taggedAddresses = ImmutableTaggedAddresses.builder()
                 .wan("172.217.17.110")
                 .build();
+
+        assertThat(taggedAddresses.getLan(), is(Optional.empty()));
+        assertThat(taggedAddresses.getWan(), is("172.217.17.110"));
     }
 
     @Test(expected = IllegalStateException.class)
--- src/test/java/com/orbitz/consul/model/query/FailoverTest.java
+++ src/test/java/com/orbitz/consul/model/query/FailoverTest.java
@@ -1,29 +1,42 @@
 package com.orbitz.consul.model.query;
 
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+
 import com.google.common.collect.Lists;
+
 import org.junit.Test;
+
+import java.util.Optional;
 
 public class FailoverTest {
 
     @Test
     public void creatingFailoverWithDatacentersIsValid() {
-        ImmutableFailover.builder()
+        ImmutableFailover failover = ImmutableFailover.builder()
                 .datacenters(Lists.newArrayList("dc1", "dc2"))
                 .build();
+
+        assertThat(failover.datacenters(), is(Optional.of(Lists.newArrayList("dc1", "dc2"))));
     }
 
     @Test
     public void creatingFailoverWithNearestIsValid() {
-        ImmutableFailover.builder()
+        ImmutableFailover failover = ImmutableFailover.builder()
                 .nearestN(2)
                 .build();
+
+        assertThat(failover.getNearestN(), is(Optional.of(2)));
     }
 
     @Test
     public void creatingFailoverWithNearestAndDatacentersIsValid() {
-        ImmutableFailover.builder()
+        ImmutableFailover failover = ImmutableFailover.builder()
                 .datacenters(Lists.newArrayList("dc1", "dc2"))
                 .nearestN(2)
                 .build();
+
+        assertThat(failover.datacenters(), is(Optional.of(Lists.newArrayList("dc1", "dc2"))));
+        assertThat(failover.getNearestN(), is(Optional.of(2)));
     }
 }
