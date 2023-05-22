7d50c63b321ca22c3a32d15e8c31905396c382f8
Scott Leberknight <174812+sleberknight@users.noreply.github.com>
2023-05-22 13:32:24 -0400

    Fix PolicyResponse de-serialization errors (#97)
    * Change the type of BasePolicyResponse#datacenters from
      Optional<String> to Optional<List<String>>
    * Add new test in AclTest to verify JSON de-serialization when
      the returned policy contains data centers
    * Even though I don't think Policy is ever de-serialized (since it
      is used when creating new policies), remove the JsonSerialization
      annotation because it tells Jackson to de-serialize as an
      ImmutableList, which will fail because that is not a subtype, and
      is thus not convertible to, an Optional<List<String>>
    * Change from star import to listing all imports explicitly in AclTest, and
      change assertThat to be from MatcherAssert because the one in
      Assert is deprecated
    
    Fixes #96

--- src/main/java/com/orbitz/consul/model/acl/Policy.java
+++ src/main/java/com/orbitz/consul/model/acl/Policy.java
@@ -4,7 +4,6 @@ import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
 import com.fasterxml.jackson.databind.annotation.JsonSerialize;
-import com.google.common.collect.ImmutableList;
 import org.immutables.value.Value;
 
 import java.util.List;
@@ -29,7 +28,6 @@ public abstract class Policy {
     public abstract Optional<String> rules();
 
     @JsonProperty("Datacenters")
-    @JsonDeserialize(as = ImmutableList.class, contentAs = String.class)
     public abstract Optional<List<String>> datacenters();
 
 }
