9733637bb37be4d1da902fd44538666bc3c6eecc
Scott Leberknight <174812+sleberknight@users.noreply.github.com>
2023-05-31 12:49:00 -0400

    Add no-op implementation of ClientEventCallback (#161)
    * Add NoOpClientEventCallback
    
    Closes  #160

---
+++ src/main/java/com/orbitz/consul/monitoring/NoOpClientEventCallback.java
@@ -0,0 +1,7 @@
+package com.orbitz.consul.monitoring;
+
+/**
+ * A "no-op" implementation of {@link ClientEventCallback}.
+ */
+public class NoOpClientEventCallback implements ClientEventCallback {
+}
