7278fe77ac0855dc110ef6def4c202706d1d3c31
Scott Leberknight <174812+sleberknight@users.noreply.github.com>
2023-06-08 18:25:30 +0000

    Extract JSON from Java test code into resource file
    Since we're still limite to Java 11, we can't use text blocks, so
    the next best thing is to extract this gigantic blob of string
    concatenation into a .json file in src/test/resources and load
    it from the test.
    
    I copied and slightly modified code from kiwi-tests's Fixtures
    class since this library does not yet depend on other kiwi projects.

--- src/test/java/com/orbitz/consul/TestUtils.java
+++ src/test/java/com/orbitz/consul/TestUtils.java
@@ -1,5 +1,11 @@
 package com.orbitz.consul;
 
+import com.google.common.io.Resources;
+
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.UUID;
 
 public class TestUtils {
@@ -10,5 +16,29 @@ public class TestUtils {
 
     public static String randomUUIDString() {
         return UUID.randomUUID().toString();
+    }
+
+    public static String fixture(String resourceName) {
+        return fixture(resourceName, StandardCharsets.UTF_8);
+    }
+
+    public static String fixture(String resourceName, Charset charset) {
+        Throwable t;
+        try {
+            var url = Resources.getResource(resourceName);
+            var path = Paths.get(url.toURI());
+            return Files.readString(path, charset);
+        } catch (Exception e) {
+            t = e;
+        } catch (Error e) {
+            // Because of bug JDK-8286287, we must catch Error. This bug is fixed in JDK 19.
+            // See also:
+            // https://bugs.openjdk.org/browse/JDK-8286287
+            // https://stackoverflow.com/questions/72127702/error-which-shouldnt-happen-caused-by-malformedinputexception-when-reading-fi
+            t = e;
+        }
+
+        var message = String.format("Unable to load resource %s using charset %s", resourceName, charset);
+        throw new RuntimeException(message, t);
     }
 }
