2cd70320d53b3850fce23519817deceb262dbcae
Scott Leberknight <174812+sleberknight@users.noreply.github.com>
2023-07-03 12:25:14 -0400

    Update Docker image name for Consul to hashicorp/consul:1.15
    This is because of the notice posted to hub.docker.com:
    
    Upcoming in Consul 1.16, we will stop publishing official Dockerhub
    images and publish only our Verified Publisher images. Users of Docker
    images should pull from hashicorp/consul instead of consul. Verified
    Publisher images can be found at
    https://hub.docker.com/r/hashicorp/consul.

---
+++ src/itest/java/org/kiwiproject/consul/ConsulTestcontainers.java
@@ -0,0 +1,10 @@
+package org.kiwiproject.consul;
+
+public class ConsulTestcontainers {
+
+    private ConsulTestcontainers() {
+        // utility class
+    }
+
+    public static final String CONSUL_DOCKER_IMAGE_NAME = "hashicorp/consul:1.15";
+}
