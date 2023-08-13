8d552f8ad82fc54d20c23c4fce315576c15e8876
Scott Leberknight <174812+sleberknight@users.noreply.github.com>
2023-08-13 18:23:56 -0400

    Deprecate TokenType for removal (#258)
    It is deprecated since 1.1.0, and will be removed in a future
    major version since it changes the public API (even with it being
    unused, we should follow correct SemVer semantics)
    
    Closes #257

--- src/main/java/org/kiwiproject/consul/model/acl/TokenType.java
+++ src/main/java/org/kiwiproject/consul/model/acl/TokenType.java
@@ -1,5 +1,10 @@
 package org.kiwiproject.consul.model.acl;
 
+/**
+ * @deprecated for removal without replacement, since this was part of the legacy ACL system. For more details see
+ * comments in <a href="https://github.com/kiwiproject/consul-client/issues/185">Investigate: TokenType enum is not used</a>
+ */
+@Deprecated(since = "1.1.0", forRemoval = true)
 public enum TokenType {
 
     CLIENT("client"), MANAGEMENT("management");
