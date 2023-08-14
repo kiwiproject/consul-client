10dd10a6bc0fb0aaedcaa30e51df3a3d0ce60b4a
Scott Leberknight <174812+sleberknight@users.noreply.github.com>
2023-08-14 19:11:11 -0400

    Add additional tests for Options
    These are trivial, but should be explicitly tested.

--- src/test/java/org/kiwiproject/consul/option/OptionsTest.java
+++ src/test/java/org/kiwiproject/consul/option/OptionsTest.java
@@ -2,9 +2,17 @@ package org.kiwiproject.consul.option;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import org.apache.commons.lang3.RandomStringUtils;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
 
 @DisplayName("Options")
 class OptionsTest {
@@ -45,6 +53,67 @@ class OptionsTest {
             var options = Options.from(null, queryOptions, null);
 
             assertThat(options).containsAllEntriesOf(queryOptions.toQuery());
+        }
+    }
+
+    @Nested
+    class OptionallyAddToMap {
+
+        private Map<String, Object> data;
+        private String key;
+
+        @BeforeEach
+        void setUp() {
+            data = new HashMap<>();
+            key = RandomStringUtils.randomAlphabetic(10);
+        }
+
+        @Test
+        void shouldDoNothing_WhenOptionalIsEmpty() {
+            var value = Optional.empty();
+            Options.optionallyAdd(data, key, value);
+            assertThat(data).isEmpty();
+        }
+
+        @Test
+        void shouldPutValueAsString_ForKey_WhenOptionalContainsValue() {
+            var value = Optional.of(42);
+            Options.optionallyAdd(data, key, value);
+            assertThat(data).hasSize(1).containsEntry(key, "42");
+        }
+    }
+
+    @Nested
+    class OptionallyAddToList {
+
+        private List<String> data;
+        private String key;
+
+        @BeforeEach
+        void setUp() {
+            data = new ArrayList<>();
+            key = RandomStringUtils.randomAlphabetic(10);
+        }
+
+        @Test
+        void shouldDoNothing_WhenOptionalIsEmpty() {
+            var value = Optional.<Boolean>empty();
+            Options.optionallyAdd(data, key, value);
+            assertThat(data).isEmpty();
+        }
+
+        @Test
+        void shouldDoNothing_WhenOptionalContainsFalseBoolean() {
+            var value = Optional.of(false);
+            Options.optionallyAdd(data, key, value);
+            assertThat(data).isEmpty();
+        }
+
+        @Test
+        void shouldAddKey_WhenOptionalContainsTrueBoolean() {
+            var value = Optional.of(true);
+            Options.optionallyAdd(data, key, value);
+            assertThat(data).containsExactly(key);
         }
     }
 }
