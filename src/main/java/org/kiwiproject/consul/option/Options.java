7f35874e0dfcbccede3ba7000b613decd26b47b6
Scott Leberknight <174812+sleberknight@users.noreply.github.com>
2024-10-09 17:30:46 -0400

    Refactor Options#from to streams and add javadocs (#378)
    Closes #376
    Closes #377

--- src/main/java/org/kiwiproject/consul/option/Options.java
+++ src/main/java/org/kiwiproject/consul/option/Options.java
@@ -1,10 +1,13 @@
 package org.kiwiproject.consul.option;
 
+import static com.google.common.base.Preconditions.checkArgument;
 import static java.util.Objects.nonNull;
+import static java.util.stream.Collectors.toMap;
 
-import java.util.HashMap;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
+import java.util.Objects;
 import java.util.Optional;
 
 /**
@@ -34,23 +37,53 @@ public class Options {
         // utility class
     }
 
-    static void optionallyAdd(Map<String, Object> data, String key, @SuppressWarnings("OptionalUsedAsFieldOrParameterType") Optional<?> val) {
+    /**
+     * If the given Optional {@code val} contains a value, then a new entry is added to
+     * {@code data} using {@code key} as the key and the value in {@code val} as the value.
+     * <p>
+     * The {@code data} Map <em>must</em> be mutable.
+     *
+     * @param data the map into which a new entry will be added
+     * @param key the key for the new entry
+     * @param val an Optional that may contain a value
+     * @throws UnsupportedOperationException if {@code val} contains a value but {@code data} is an unmodifiable Map
+     */
+    static void optionallyAdd(Map<String, Object> data,
+                              String key,
+                              @SuppressWarnings("OptionalUsedAsFieldOrParameterType") Optional<?> val) {
         val.ifPresent(value -> data.put(key, value.toString()));
     }
 
+    /**
+     * Convert one or more options to a map.
+     *
+     * @param options the options to convert
+     * @return a map containing the aggregated query options
+     */
     public static Map<String, Object> from(ParamAdder... options) {
-        Map<String, Object> result = new HashMap<>();
-
-        for (ParamAdder adder : options) {
-            if (nonNull(adder)) {
-                result.putAll(adder.toQuery());
-            }
-        }
-
-        return result;
+        checkArgument(nonNull(options), "the options vararg must not be null");
+        return Arrays.stream(options)
+                .filter(Objects::nonNull)
+                .map(ParamAdder::toQuery)
+                .flatMap(m -> m.entrySet().stream())
+                .collect(toMap(Map.Entry::getKey, Map.Entry::getValue));
     }
 
-    static void optionallyAdd(List<String> data, String key, @SuppressWarnings("OptionalUsedAsFieldOrParameterType") Optional<Boolean> val) {
+    /**
+     * If the given Optional {@code val} contains a {@code true} Boolean value, then add {@code key}
+     * as a new element to {@code data}.
+     * <p>
+     * The {@code data} List <em>must</em> be mutable.
+     *
+     * @param data the list into which a new element will be added
+     * @param key the value to add to the list
+     * @param val a "flag" indicating whether the value of {@code key} should be added
+     * @throws UnsupportedOperationException if {@code val} contains a {@code true} value
+     * but {@code data} is an unmodifiable List
+     */
+    static void optionallyAdd(List<String> data,
+                              String key,
+                              @SuppressWarnings("OptionalUsedAsFieldOrParameterType") Optional<Boolean> val) {
         val.ifPresent(value -> {
             if (value) {
                 data.add(key);
--- src/test/java/org/kiwiproject/consul/option/OptionsTest.java
+++ src/test/java/org/kiwiproject/consul/option/OptionsTest.java
@@ -1,6 +1,8 @@
 package org.kiwiproject.consul.option;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
+import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
 
 import org.apache.commons.lang3.RandomStringUtils;
 import org.junit.jupiter.api.BeforeEach;
@@ -21,8 +23,29 @@ class OptionsTest {
     class From {
 
         @Test
+        void shouldThrowIllegalArgument_WhenGivenNullVarArgs() {
+            assertThatIllegalArgumentException()
+                    .isThrownBy(() -> Options.from((ParamAdder[]) null))
+                    .withMessage("the options vararg must not be null");
+        }
+
+        @Test
         void shouldReturnEmptyMap_WhenGivenNoOptions() {
             assertThat(Options.from()).isEmpty();
+        }
+
+        @Test
+        void shouldReturnEmptyMap_WhenOneNullArgument() {
+            var options = Options.from((ParamAdder) null);
+
+            assertThat(options).isEmpty();
+        }
+
+        @Test
+        void shouldReturnEmptyMap_WhenAllOptionsAreNull() {
+            var options = Options.from(null, null);
+
+            assertThat(options).isEmpty();
         }
 
         @Test
@@ -81,6 +104,14 @@ class OptionsTest {
             Options.optionallyAdd(data, key, value);
             assertThat(data).hasSize(1).containsEntry(key, "42");
         }
+
+        @Test
+        void shouldThrowUnsupportedOperationException_WhenGivenUnmodifiableMap() {
+            var unmodifiableData = Map.<String, Object>of();
+            var optionalValue = Optional.of(42);
+            assertThatExceptionOfType(UnsupportedOperationException.class)
+                    .isThrownBy(() -> Options.optionallyAdd(unmodifiableData, "theKey", optionalValue));
+        }
     }
 
     @Nested
@@ -114,6 +145,14 @@ class OptionsTest {
             var value = Optional.of(true);
             Options.optionallyAdd(data, key, value);
             assertThat(data).containsExactly(key);
+        }
+
+        @Test
+        void shouldThrowUnsupportedOperationException_WhenGivenUnmodifiableList() {
+            var unmodifiableData = List.<String>of();
+            var optionalValue = Optional.of(true);
+            assertThatExceptionOfType(UnsupportedOperationException.class)
+                    .isThrownBy(() -> Options.optionallyAdd(unmodifiableData, "theKey", optionalValue));
         }
     }
 }
