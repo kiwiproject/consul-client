cae9704951e5ddc6f9caab34520fd80166dbfa5b
Scott Leberknight <174812+sleberknight@users.noreply.github.com>
2025-01-06 19:33:24 -0500

    Add isNotNullOrEmpty to Lists (#408)
    Maybe we need to start depending on our own kiwi project, since it
    already has a lot of the utilities we need, such as this one.
    
    This method is going to be used in the round-robin
    ConsulFailoverStrategy implementation.
    
    Relates to #397

--- src/main/java/org/kiwiproject/consul/util/Lists.java
+++ src/main/java/org/kiwiproject/consul/util/Lists.java
@@ -5,6 +5,9 @@ import static java.util.Objects.isNull;
 import java.util.List;
 import java.util.Optional;
 
+/**
+ * Static utilities related to {@link List}.
+ */
 public class Lists {
 
     private Lists() {
@@ -13,6 +16,10 @@ public class Lists {
 
     public static <T> Optional<T> firstValueOrEmpty(List<T> list) {
         return isNullOrEmpty(list) ? Optional.empty() : Optional.of(list.get(0));
+    }
+
+    public static <T> boolean isNotNullOrEmpty(List<T> list) {
+        return !isNullOrEmpty(list);
     }
 
     public static <T> boolean isNullOrEmpty(List<T> list) {
