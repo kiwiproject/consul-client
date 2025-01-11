34725d16e3f9234473d2ea2b7b46df66cb5b375a
Scott Leberknight <174812+sleberknight@users.noreply.github.com>
2025-01-11 13:00:36 -0500

    Add round-robin implementation of ConsulFailoverStrategy
    * Add RoundRobinConsulFailoverStrategy with optional delay after
      failed requests. It just keeps picking the next target server
      in a "circular list" fashion. It uses an AtomicInteger to
      store the index of the last failed target, and is therefore
      thread-safe. We don't really need to care whether multiple
      threads get each target in order, as long as they get a
      different one on each request. But in some limited functional
      testing, I found that multiple threads using the same Consul
      instance all do go in order, i.e., 1 to 2 to 3 back to 1, etc.
    * The tests are a mix of static (i.e., hard-coding URLs) and
      dynamic (i.e., using ParameterizedTest). The reason is mainly
      that I originally did all the static tests, then decided to
      add the dynamic ones, but decided to keep the static data tests
      since they are a bit easier to read. But, since I also like the
      dynamic tests, I kept all of them.
    
    Closes #397

---
+++ src/main/java/org/kiwiproject/consul/util/failover/strategy/RoundRobinConsulFailoverStrategy.java
@@ -0,0 +1,116 @@
+package org.kiwiproject.consul.util.failover.strategy;
+
+import static com.google.common.base.Preconditions.checkArgument;
+import static java.util.Objects.nonNull;
+import static org.apache.commons.lang3.ThreadUtils.sleepQuietly;
+import static org.kiwiproject.consul.util.HostAndPorts.hostAndPortFromOkHttpRequest;
+import static org.kiwiproject.consul.util.Lists.isNotNullOrEmpty;
+
+import com.google.common.annotations.VisibleForTesting;
+import com.google.common.net.HostAndPort;
+import okhttp3.HttpUrl;
+import okhttp3.Request;
+import okhttp3.Response;
+import org.checkerframework.checker.nullness.qual.NonNull;
+import org.checkerframework.checker.nullness.qual.Nullable;
+
+import java.time.Duration;
+import java.util.List;
+import java.util.Optional;
+import java.util.concurrent.atomic.AtomicInteger;
+
+/**
+ * A {@link ConsulFailoverStrategy} that round-robins a list of Consul servers.
+ */
+public class RoundRobinConsulFailoverStrategy implements ConsulFailoverStrategy {
+
+    // set to invalid index for initial state, so that we always try the first target initially
+    @VisibleForTesting
+    final AtomicInteger lastTargetIndex = new AtomicInteger(-1);
+
+    private final List<HostAndPort> targets;
+    private final int numberOfTargets;
+    private final Duration delay;
+    private final boolean delayAfterFailedRequest;
+
+    /**
+     * Create a new instance with the given list of Consul target servers.
+     *
+     * @param targets the Consul servers
+     */
+    public RoundRobinConsulFailoverStrategy(List<HostAndPort> targets) {
+        this(targets, Duration.ZERO);
+    }
+
+    /**
+     * Create a new instance with the given list of Consul target servers.
+     * <p>
+     * After each failed request, the instance will sleep for the given delay.
+     *
+     * @param targets           the Consul servers
+     * @param delayAfterFailure the amount to sleep after a failed request
+     */
+    public RoundRobinConsulFailoverStrategy(List<HostAndPort> targets, Duration delayAfterFailure) {
+        checkArgument(isNotNullOrEmpty(targets), "targets must not be null or empty");
+        this.targets = List.copyOf(targets);
+        this.numberOfTargets = targets.size();
+
+        checkArgument(nonNull(delayAfterFailure), "delayAfterFailure must not be null");
+        var millis = delayAfterFailure.toMillis();
+        checkArgument(millis >= 0, "delayAfterFailure must be zero or a positive duration");
+        this.delay = delayAfterFailure;
+        this.delayAfterFailedRequest = millis > 0;
+    }
+
+    @Override
+    @NonNull
+    public Optional<Request> computeNextStage(Request previousRequest) {
+        return computeNextStage(previousRequest, null);
+    }
+
+    @SuppressWarnings("removal")
+    @Override
+    @NonNull
+    public Optional<Request> computeNextStage(@NonNull Request previousRequest, @Nullable Response previousResponse) {
+        var nextTarget = hostAndPortFromOkHttpRequest(previousRequest);
+        var indexOfNextTarget = targets.indexOf(nextTarget);
+
+        if (lastTargetIndex.get() == indexOfNextTarget) {
+            var nextIndex = nextCircularListIndex(indexOfNextTarget);
+
+            lastTargetIndex.set(nextIndex);
+
+            nextTarget = targets.get(nextIndex);
+
+            sleepIfPositiveDelay();
+        }
+
+        HttpUrl nextURL = previousRequest.url().newBuilder()
+                .host(nextTarget.getHost())
+                .port(nextTarget.getPort())
+                .build();
+        return Optional.of(previousRequest.newBuilder().url(nextURL).build());
+    }
+
+    private int nextCircularListIndex(int currentIndex) {
+        return (currentIndex + 1) % numberOfTargets;
+    }
+
+    private void sleepIfPositiveDelay() {
+        if (delayAfterFailedRequest) {
+            sleepQuietly(delay);
+        }
+    }
+
+    @Override
+    public boolean isRequestViable(@NonNull Request request) {
+        // always viable since we will move to the next target after a failure
+        return true;
+    }
+
+    @Override
+    public void markRequestFailed(@NonNull Request request) {
+        var hostAndPort = hostAndPortFromOkHttpRequest(request);
+        lastTargetIndex.set(targets.indexOf(hostAndPort));
+    }
+}
