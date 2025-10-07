9a3b6da1898d9e981c8e724a31f99b8e5475ab80
Scott Leberknight <174812+sleberknight@users.noreply.github.com>
2025-10-06 21:42:16 -0400

    Remove unused import

--- src/test/java/org/kiwiproject/consul/util/HttpTest.java
+++ src/test/java/org/kiwiproject/consul/util/HttpTest.java
@@ -27,7 +27,6 @@ import retrofit2.Response;
 
 import java.io.IOException;
 import java.math.BigInteger;
-import java.util.Map;
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicInteger;
