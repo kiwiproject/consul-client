8ddc4afeeb16cfa45c322a7e644a266a22fbcfb5
Scott Leberknight <174812+sleberknight@users.noreply.github.com>
2025-10-11 11:15:51 -0400

    Populate lastCacheInfo with cache headers from ConsulResponse
    - Set lastCacheInfo using cache headers in ConsulResponse.
    - Add tests to verify cache header information populates correctly.
    - Add constructor to StubCallbackConsumer that accepts cache hit/miss
      and age header values.
    - Add TRACE-level logging in scheduleRunCallbackSafely to see
      information about the next schedule and delay, and also when
      we do not schedule because the cache isn't running.
    - Increase the test log level to TRACE to see the new TRACE-level
      logging when scheduling the next callback.

--- src/test/java/org/kiwiproject/consul/cache/StubCallbackConsumer.java
+++ src/test/java/org/kiwiproject/consul/cache/StubCallbackConsumer.java
@@ -10,16 +10,24 @@ import java.util.List;
 public class StubCallbackConsumer implements ConsulCache.CallbackConsumer<Value> {
 
     private final List<Value> result;
+    private final String cacheHeader;
+    private final String ageHeader;
     private int callCount;
 
     public StubCallbackConsumer(List<Value> result) {
+        this(result, null, null);
+    }
+
+    public StubCallbackConsumer(List<Value> result, String cacheHeader, String ageHeader) {
         this.result = List.copyOf(result);
+        this.cacheHeader = cacheHeader;
+        this.ageHeader = ageHeader;
     }
 
     @Override
     public void consume(BigInteger index, ConsulResponseCallback<List<Value>> callback) {
         callCount++;
-        callback.onComplete(new ConsulResponse<>(result, 0, true, BigInteger.ZERO, null, null));
+        callback.onComplete(new ConsulResponse<>(result, 0, true, BigInteger.ZERO, cacheHeader, ageHeader));
     }
 
     public int getCallCount() {
