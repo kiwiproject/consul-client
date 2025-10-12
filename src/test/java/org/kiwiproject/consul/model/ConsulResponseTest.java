320ee12499ff2a07a000468b202e62cc555ad587
Scott Leberknight <174812+sleberknight@users.noreply.github.com>
2025-10-12 15:11:38 -0400

    Add tests for deprecated and unused ConsulResponse constructor

--- src/test/java/org/kiwiproject/consul/model/ConsulResponseTest.java
+++ src/test/java/org/kiwiproject/consul/model/ConsulResponseTest.java
@@ -1,10 +1,13 @@
 package org.kiwiproject.consul.model;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertAll;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
+
+import java.util.Optional;
 
 @DisplayName("ConsulResponse")
 class ConsulResponseTest {
@@ -36,6 +39,68 @@ class ConsulResponseTest {
             var cacheResponseInfo = cacheHitResponse.getCacheReponseInfo().orElseThrow();
             assertThat(cacheResponseInfo.isCacheHit()).isTrue();
             assertThat(cacheResponseInfo.getAgeInSeconds()).contains(42L);
+        }
+
+        @SuppressWarnings("removal")
+        @Test
+        void shouldCreateUsingDeprecatedConstructor_WhenCacheResponseInfoExists() {
+            var cacheHitResponse = new ConsulResponse<>(null,
+                    0,
+                    false,
+                    null,
+                    Optional.ofNullable(ConsulResponse.buildCacheResponseInfo("HIT", "0")));
+
+            assertAll(
+                    () -> assertThat(cacheHitResponse.getResponse()).isNull(),
+                    () -> assertThat(cacheHitResponse.getLastContact()).isZero(),
+                    () -> assertThat(cacheHitResponse.isKnownLeader()).isFalse(),
+                    () -> assertThat(cacheHitResponse.getIndex()).isNull(),
+                    () -> assertThat(cacheHitResponse.getCacheResponseInfo()).isPresent()
+            );
+        }
+
+        @SuppressWarnings("removal")
+        @Test
+        void shouldCreateUsingDeprecatedConstructor_WhenCacheResponseInfo_DoesNotExist() {
+            var cacheHitResponse = new ConsulResponse<>(null,
+                    0,
+                    false,
+                    null,
+                    Optional.ofNullable(ConsulResponse.buildCacheResponseInfo(null, null)));
+
+            assertAll(
+                    () -> assertThat(cacheHitResponse.getResponse()).isNull(),
+                    () -> assertThat(cacheHitResponse.getLastContact()).isZero(),
+                    () -> assertThat(cacheHitResponse.isKnownLeader()).isFalse(),
+                    () -> assertThat(cacheHitResponse.getIndex()).isNull(),
+                    () -> assertThat(cacheHitResponse.getCacheResponseInfo()).isEmpty()
+            );
+        }
+    }
+
+    @Nested
+    class BuildCacheResponseInfo {
+
+        @Test
+        void shouldReturnNull_WhenHitMissCacheHeader_IsNull() {
+            assertThat(ConsulResponse.buildCacheResponseInfo(null, null)).isNull();
+        }
+
+        @Test
+        void shouldReturnValue_ForCacheHit() {
+            var cacheResponseInfo = ConsulResponse.buildCacheResponseInfo("HIT", "0");
+            assertThat(cacheResponseInfo).isNotNull();
+
+            assertAll(
+                    () -> assertThat(cacheResponseInfo.isCacheHit()).isTrue(),
+                    () -> assertThat(cacheResponseInfo.getAgeInSeconds()).contains(0L)
+            );
+        }
+
+        @Test
+        void shouldReturnValue_ForCacheMiss() {
+            var cacheResponseInfo = ConsulResponse.buildCacheResponseInfo(null, null);
+            assertThat(cacheResponseInfo).isNull();
         }
     }
 }
