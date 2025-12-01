41ebd3faf77eb84daad078fa9aa53222c144e169
Scott Leberknight <174812+sleberknight@users.noreply.github.com>
2025-11-30 20:53:07 -0500

    Replace usage of deprecated URL constructors in Urls (#517)
    All URL constructors are deprecated. You are supposed to create a URI
    and then convert to URL using toURL(). This changes the Urls utility
    class to construct URIs and then convert using toURL(). We also have to
    handle additional exceptions that we didn't previously, mainly
    URISyntaxException thrown from URI constructors. We still need to handle
    MalformedURLException since toURL() throws it.
    
    Additionally, we were using the URL constructor:
    
    URL(String protocol, String host, int port, String file)
    
    This takes a "file" which has a very specific meaning in the URL class.
    It includes the path, query, and fragment. There is no equivalent URI
    constructor. Instead, the comparable URI constructor accepts the path,
    query, and fragment as separate arguments.
    
    To keep the functionality the same, we need to parse the "file" argument
    into separate path, query, and fragment values, and then pass those to
    the URI constructor. To handle URISyntaxException, the
    UncheckedURISyntaxException class from kiwi is copied here and thrown
    when there is a URI syntax error. It wraps the checked
    URISyntaxException.
    
    Last, added Nullable annotation to the "file" argument in the newUrl
    method that has "file" as its last argument.

--- src/test/java/org/kiwiproject/consul/util/UrlsTest.java
+++ src/test/java/org/kiwiproject/consul/util/UrlsTest.java
@@ -1,9 +1,14 @@
 package org.kiwiproject.consul.util;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThatExceptionOfType;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.NullAndEmptySource;
+import org.junit.jupiter.params.provider.ValueSource;
 
 class UrlsTest {
 
@@ -16,8 +21,20 @@ class UrlsTest {
     }
 
     @Test
-    void shouldCreateNewUrl_FromString_AndThrow_WhenMalformed() {
-        assertThatExceptionOfType(UncheckedMalformedURLException.class).isThrownBy(() -> Urls.newUrl("oops"));
+    void createNewUrl_FromString_ShouldThrow_WhenNotAbsolute() {
+        assertThatIllegalArgumentException().isThrownBy(() -> Urls.newUrl("oops"));
+    }
+
+    @Test
+    void createNewUrl_FromString_ShouldThrow_WhenBadURISyntax() {
+        assertThatExceptionOfType(UncheckedURISyntaxException.class)
+                .isThrownBy(() -> Urls.newUrl("bad_protocol://acme.com:9876"));
+    }
+
+    @Test
+    void createNewUrl_FromString_ShouldThrow_WhenMalformed() {
+        assertThatExceptionOfType(UncheckedMalformedURLException.class)
+                .isThrownBy(() -> Urls.newUrl("nosuchscheme://example.com"));
     }
 
     @Test
@@ -28,7 +45,51 @@ class UrlsTest {
     }
 
     @Test
-    void shouldCreateNewUrl_FromComponents_AndThrow_WhenMalformed() {
-        assertThatExceptionOfType(UncheckedMalformedURLException.class).isThrownBy(() -> Urls.newUrl("bad_protocol", "github.com", 8080));
+    void createNewUrl_FromString_ShouldThrow_WhenPathNotAbsolute() {
+        assertThatExceptionOfType(UncheckedURISyntaxException.class)
+                .isThrownBy(() -> Urls.newUrl("https", "acme.com", 61000, "path"));
+    }
+
+    @ParameterizedTest
+    @NullAndEmptySource
+    @ValueSource(strings = { " ", "  " })
+    void shouldCreateNewUrl_FromComponents_AndNoFile(String file) {
+        var url = Urls.newUrl("https", "acme.com", 443, file);
+
+        assertThat(url).hasToString("https://acme.com:443");
+    }
+
+    @Test
+    void shouldCreateNewUrl_FromComponents_AndFile() {
+        var url = Urls.newUrl("https", "acme.com", 443, "/some-path");
+
+        assertThat(url).hasToString("https://acme.com:443/some-path");
+    }
+
+    @ParameterizedTest
+    @CsvSource(textBlock = """
+            /foo
+            /foo?bar=baz
+            /foo?bar=baz&a=b
+            /foo#a1
+            /foo#b1
+            /foo?bar=baz#a1
+            """)
+    void shouldCreateNewUrl_FromVariousFileArguments(String file) {
+        var url = Urls.newUrl("https", "acme.com", 8443, file);
+
+        assertThat(url).hasToString("https://acme.com:8443" + file);
+    }
+
+    @Test
+    void createNewUrl_FromComponents_ShouldThrow_WhenBadURISyntax() {
+        assertThatExceptionOfType(UncheckedURISyntaxException.class)
+                .isThrownBy(() -> Urls.newUrl("bad_protocol", "github.com", 8080));
+    }
+
+    @Test
+    void createNewUrl_FromComponents_ShouldThrow_WhenMalformed() {
+        assertThatExceptionOfType(UncheckedMalformedURLException.class)
+                .isThrownBy(() -> Urls.newUrl("nosuchscheme", "acme.com", 9876));
     }
 }
