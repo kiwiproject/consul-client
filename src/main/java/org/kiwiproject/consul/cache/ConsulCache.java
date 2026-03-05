f5291b324f12f2dfd784d82b400c66ba69857c51
Gaurav Tayal <gauravtayal0@gmail.com>
2026-03-05 07:08:40 +0530

    moving filter option into the builder

--- src/main/java/org/kiwiproject/consul/cache/ConsulCache.java
+++ src/main/java/org/kiwiproject/consul/cache/ConsulCache.java
@@ -444,12 +444,11 @@ public class ConsulCache<K, V> implements AutoCloseable {
                 .token(queryOptions.getToken())
                 .consistencyMode(queryOptions.getConsistencyMode())
                 .near(queryOptions.getNear())
+                .filter(queryOptions.getFilter())
                 .datacenter(queryOptions.getDatacenter());
         for (String tag : queryOptions.getTag()) {
             builder.addTag(tag);
         }
-
-        queryOptions.getFilter().ifPresent(builder::filter);
 
         return builder.build();
     }
